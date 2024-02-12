c252203692c703b7e88b1f00f69b35e12a5c1c3c
Philip Brocoum <philip.brocoum@gmail.com>
2024-02-12 07:23:31 -0800

    Double check keycode.

--- client_win-mac-nix/hotkey_nix.py
+++ client_win-mac-nix/hotkey_nix.py
@@ -1,6 +1,9 @@
 from Xlib import X, display
 from time import time
 
+# F7
+KEYCODE = 73
+
 my_display = display.Display()
 my_screen = my_display.screen()
 my_root = my_screen.root
@@ -13,11 +16,15 @@ def hotkey_run(callback=None):
 
     # we tell the X server we want to catch keyPress event
     my_root.change_attributes(event_mask=X.KeyPressMask)
-    # Shift + F7
-    my_root.grab_key(73, X.AnyModifier, True, X.GrabModeAsync, X.GrabModeAsync)
+    my_root.grab_key(KEYCODE, X.AnyModifier, True,
+                     X.GrabModeAsync, X.GrabModeAsync)
 
     while True:
         my_event = my_display.next_event()
+        if ("detail" not in my_event._data):
+            continue
+        if (my_event.detail != KEYCODE):
+            continue
         now = time()
         if now - hotkey_time > 0.25:
             hotkey_time = now
