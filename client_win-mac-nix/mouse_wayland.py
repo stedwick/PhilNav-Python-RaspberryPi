04665bbd06d9df2b9bc095f60b082f833e19a4e2
Philip Brocoum <philip.brocoum@gmail.com>
2024-12-08 18:49:37 -0800

    [cursor] A few important notes about this implementation

--- client_win-mac-nix/mouse_wayland.py
+++ client_win-mac-nix/mouse_wayland.py
@@ -36,3 +36,23 @@ def setCursorPos(x, y):
     device.emit(uinput.REL_Y, dy)
     # Small sleep to ensure events are processed
     time.sleep(0.01)
+
+
+# Prompt: I'm making a virtual mouse in Python, and I have implementations for Windows, Mac, and Linux X11, see attached files. Linux is moving to Wayland which doesn't support X11 protocols to move the mouse, so I want to write an implementation using python-uinput. See https://pypi.org/project/python-uinput/ and https://github.com/pyinput/python-uinput.
+
+# Create a setCursorPos function for me using /dev/uinput thanks!
+
+# I'll help you create a Wayland implementation using python-uinput. Note that using uinput requires root privileges or adding the user to the input group.
+# Here's the implementation for mouse_wayland.py:
+
+# A few important notes about this implementation:
+# This requires root privileges or the user needs to be in the input group
+# You'll need to install python-uinput (pip install python-uinput) and the system package (sudo apt-get install python3-uinput on Ubuntu/Debian)
+# The implementation uses relative movements since uinput doesn't support absolute positioning
+# Position tracking might get out of sync with the actual cursor position since we can't read the true position
+# The time.sleep() is added to ensure events are processed properly
+# To use this on a Linux system with Wayland, you'll need to:
+# Install the requirements: sudo apt-get install libudev-dev
+# Add your user to the input group: sudo usermod -a -G input $USER
+# Load the uinput kernel module: sudo modprobe uinput
+# You might want to add error handling and initialization checks to make sure the uinput device is available and properly set up.
